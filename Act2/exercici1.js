04647fd40db25ce8ad0e677a181363ad2b7d9b6b
Miilenna <milena200.fv@gmail.com>
2024-10-03 18:10:24 +0200

    condicio per espais buits

--- Act2/exercici1.js
+++ Act2/exercici1.js
@@ -1,4 +1,9 @@
 let numero = prompt("Introdueix un valor")
 
-let esNumero = !isNaN(numero) ? "És un número" : "No és un número";
-console.log(esNumero);
+if(numero) {
+    let esNumero = !isNaN(numero) ? "És un número" : "No és un número";
+    console.log(esNumero);
+} else {
+    console.log("No has introduït cap número");
+}
+
