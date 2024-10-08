670208554ca632f18c0f6326b43f01bddc9a197d
Miilenna <milena200.fv@gmail.com>
2024-10-08 18:01:54 +0200

    comentaris posats

--- Act2/exercici1.js
+++ Act2/exercici1.js
@@ -1,7 +1,8 @@
 let numero = prompt("Introdueix un valor")
 
 if(numero) {
-    let esNumero = !isNaN(numero) ? "És un número" : "No és un número";
+    //saber si es un número o no ho és
+    let esNumero = !isNaN(numero) ? "És un número" : "No és un número"; 
     console.log(esNumero);
 } else {
     console.log("No has introduït cap número");
