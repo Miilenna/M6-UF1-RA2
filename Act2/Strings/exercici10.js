51cc71fcfaf73204081b6354e3dfd1e1d99c6ccd
Miilenna <milena200.fv@gmail.com>
2024-10-08 21:15:33 +0200

    exercici 10 millorat

--- Act2/Strings/exercici10.js
+++ Act2/Strings/exercici10.js
@@ -5,7 +5,7 @@ paraulaIncluida = string.includes('javascript');
 
 //condició si la paraula està inclosa en la cadena remplaça per JS m'agrada!!
 if(paraulaIncluida) {
-    console.log(string.replace("javascript","JS m'agrada!!"));
+    console.log(string.replace(/javascript/g,"JS m'agrada!!"));
 } 
 //si no està inclosa
 else {
