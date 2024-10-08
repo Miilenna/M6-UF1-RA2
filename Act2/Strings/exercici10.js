5f2046e670cf4f627211504ed8990882d672bae5
Miilenna <milena200.fv@gmail.com>
2024-10-08 21:10:42 +0200

    exercici 10 fet i enunciat millorat

--- Act2/Strings/exercici10.js
+++ Act2/Strings/exercici10.js
@@ -3,7 +3,7 @@ let string = prompt("Posa una paraula: ");
 //guardar la paraula en una variable
 paraulaIncluida = string.includes('javascript');
 
-//condició si la paraula està inclosa en la cadena
+//condició si la paraula està inclosa en la cadena remplaça per JS m'agrada!!
 if(paraulaIncluida) {
     console.log(string.replace("javascript","JS m'agrada!!"));
 } 
