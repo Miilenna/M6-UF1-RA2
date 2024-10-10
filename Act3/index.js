08a2b790ce5a579fc90ab72bbf11159d031e0482
Miilenna <milena200.fv@gmail.com>
2024-10-10 18:52:49 +0200

    falta arreglar l'ultima funcio i continuar

--- Act3/index.js
+++ Act3/index.js
@@ -1,6 +1,9 @@
 //variables
 let paraulaSecreta = document.getElementById("paraulaSecreta");
-
+let botoContraseña = document.getElementById("mostrarContra");
+let comencar = document.getElementById("començarPartida"); 
+let linias = document.getElementById("linias");
+let resultat = ""
 
 function mostraPassw() {
     let contrasenya = paraulaSecreta;
@@ -11,16 +14,41 @@ function mostraPassw() {
     }
 }
 function comencarPartida() {
-    if(!isNaN(paraulaSecreta.value)) {
+    if(paraulaSecreta.value === "") { //arreglar
+        alert("Has d'afegir una paraula per poder començar a jugar");
+    }
+    else if(!isNaN(paraulaSecreta.value)) {
         alert("No pot ser un número");
     }
     else if((paraulaSecreta.value.length) < 3) {
         alert("La paraula secreta ha de ser més de 3 carácters");
     }
-    else if(paraulaSecreta.value.contains(" ")) { //arreglar
-        alert("Has d'afegir una paraula per poder començar a jugar");
+    else if (/[0-9]/.test(paraulaSecreta.value)){
+        alert("No pot contenir números");
+    } else {
+        botoContraseña.disabled = true;
+        comencar.disabled = true;
+        paraulaSecreta.disabled = true;
+
+
+        espaisParaulaSecreta();
     }
-    else if (paraulaSecreta.includes(isNaN).value){
-        alert("No pot contenir un número");
+} 
+
+function espaisParaulaSecreta() { //mejorar
+    if(paraulaSecreta.value.length > 3){
+        resultat = "_ ".repeat(paraulaSecreta.value.length);
+
+        linias.textContent = resultat;
+    }
+}
+
+function mostrarContenidoBoton(valor){
+    const lletraConte = valor.toUpperCase();
+
+    if(paraulaSecreta.value.toUpperCase().includes(lletraConte)) {
+        if(linias.length = paraulaSecreta.value) {
+            linias.textContent = paraulaSecreta.value; //arreglar esto
+        }
     }
 }
